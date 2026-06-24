2754d528c0f6a745cee5fed68a60493f74a01a00
Rakib Hosain <rakibhosain2302@gmail.com>
2026-06-24 15:39:45 +0600

    sidebar UI update

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -12,12 +12,14 @@
         <div class="widget-title"><span class="title-body">Latest Articles</span></div>
         @foreach ($latestPosts as $post)
             <article class="recent-post">
-                <div class="recent-post__body">
-                    <h3 class="recent-post__title ms-1"><a href="{{ route('showPost', $post->id) }}">{{ $post->title }}</a></h3>
-                </div>
                 <a href="{{ route('showPost', $post->id) }}">
-                    <img class="recent-post__thumb" src="{{ asset('storage/' . $post->images) }}" alt="{{ $post->title }}" loading="lazy" />
+                    <img class="recent-post__thumb" src="{{ asset('storage/' . $post->images) }}"
+                        alt="{{ $post->title }}" loading="lazy" />
                 </a>
+                <div class="recent-post__body">
+                    <h3 class="recent-post__title ms-1"><a
+                            href="{{ route('showPost', $post->id) }}">{{ $post->title }}</a></h3>
+                </div>
             </article>
         @endforeach
     </div>
